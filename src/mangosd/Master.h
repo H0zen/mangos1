cb035f567f0e077f3a0a57166c304498e17deaac
H0zen <wowtech@ub.ro>
2026-08-03 01:19:33 +0300

    Log movement

--- src/mangosd/Master.h
+++ src/mangosd/Master.h
@@ -81,8 +81,9 @@ class Master
         /// region. A no-op when that console is not running.
         ///
         /// @param diff How long the last world update took, in milliseconds.
+        /// @param diffMax The longest update in the reporting window.
         /// @param tick Mean interval between updates over the reporting window.
-        void PublishConsoleStatus(uint32 diff, uint32 tick);
+        void PublishConsoleStatus(uint32 diff, uint32 diffMax, uint32 tick);
 
         /// Everything that must happen after the final world tick.
         void ShutdownWorld();
